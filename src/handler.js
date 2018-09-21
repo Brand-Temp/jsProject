84016ce73d8d41d48e072ab54bba3e5dee46c471
BrandTemp <brandon.temple.97@gmail.com>
2018-09-21 14:24:53 +1000

    Replaced message string in handler with template literal.

--- src/handler.js
+++ src/handler.js
@@ -17,7 +17,7 @@ export const todo = async (event, context) => {
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'Your request todo ${event.pathParameters.id} was completed: Stasis in darkness. // Then the substanceless blue // Pour of tor and distances.',
+      message: `Your request (todo/${event.pathParameters.id}) was completed: Stasis in darkness. // Then the substanceless blue // Pour of tor and distances.`,
       input: event,
     }),
   };
