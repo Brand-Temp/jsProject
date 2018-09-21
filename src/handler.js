a5148300888e78de0d19762dcaceba92b30255c5
BrandTemp <brandon.temple.97@gmail.com>
2018-09-21 14:07:32 +1000

    Made first endpoint. /todo/{id}

--- src/handler.js
+++ src/handler.js
@@ -12,3 +12,13 @@ export const hello = async (event, context) => {
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
+
+export const todo = async (event, context) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'Stasis in darkness. // Then the substanceless blue // Pour of tor and distances.',
+      input: event,
+    }),
+  };
+};
