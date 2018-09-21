cd8c38124f1d1be44276ad033e72a03fc519168d
BrandTemp <brandon.temple.97@gmail.com>
2018-09-21 14:33:32 +1000

    Changes from PR code review by Ben.

--- src/handler.js
+++ src/handler.js
@@ -17,8 +17,7 @@ export const todo = async (event, context) => {
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: `Your request (todo/${event.pathParameters.id}) was completed: Stasis in darkness. // Then the substanceless blue // Pour of tor and distances.`,
-      input: event,
+      message: `Your request (todo/${event.pathParameters.id}) was completed :D`,
     }),
   };
 };
