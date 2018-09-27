e52fa33fa1a29c2125368e6c68c9fc28a6f4efd1
BrandTemp <brandon.temple.97@gmail.com>
2018-09-27 13:44:39 +1000

    Altered todo handler to test if {id} is numeric.

--- src/handler.js
+++ src/handler.js
@@ -9,10 +9,18 @@ export const hello = async (event) => {
 };
 
 export const todo = async (event) => {
+  if(Number.isInteger(event.pathParameters.id)) {
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: `Your request (todo/${event.pathParameters.id}) was completed :D`,
+      }),
+    };
+  }
   return {
-    statusCode: 200,
+    statusCode: 400,
     body: JSON.stringify({
-      message: `Your request (todo/${event.pathParameters.id}) was completed :D`,
+      message: `Your request (todo/${event.pathParameters.id}) was not able to be completed as the id was not a number.`,
     }),
   };
 };
