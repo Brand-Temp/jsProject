ecb8265aed35ba899eb4935db87dd09477259552
BrandTemp <brandon.temple.97@gmail.com>
2018-10-26 22:08:37 +1100

    Added function

--- src/handler.js
+++ src/handler.js
@@ -1,3 +1,4 @@
+import axios from 'axios'
 export const hello = async (event) => {
   return {
     statusCode: 200,
@@ -23,5 +24,22 @@ export const todo = async (event) => {
     body: JSON.stringify({
       message: `Your request (todo/${id}) was not able to be completed as the id was not a number.`,
     }),
+  };
+};
+
+export const api_test = async (event) => {
+  let returned_data = 0;
+  try {
+    returned_data = await axios.get('https://jsonplaceholder.typicode.com/posts/5');
+  } catch (err) {
+    returned_data = 'There was an error with the api request';
+  }
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'Here is what the api responded:',
+      data: returned_data.data.title,
+      input: event,
+    })
   };
 };
